233ee7a189d889298c1447a6aba9e1df2990a595
Filipp <angryfriends93@gmail.com>
2021-06-10 00:06:29 +0700

    Term Papers
    Term Papers

---
+++ Term Papers/12)Palindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово для проверки на палиндром:");
+            string s = Console.ReadLine();
+            bool isPalindrom = false;
+            for (int i = 0;i<s.Length/2;++i) 
+            {
+                char firstS = char.ToLower(s[i]);
+                char secondS = char.ToLower(s[s.Length - i - 1]);
+                if (firstS == secondS)
+                {
+                    isPalindrom = true;
+                }
+                else 
+                {
+                    isPalindrom = false;
+                }
+            }
+            if (isPalindrom)
+            {
+                Console.WriteLine("Ваше слово является палиндромом");
+            }
+            else 
+            {
+                Console.WriteLine("Ваше слово не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
